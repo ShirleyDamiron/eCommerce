2f2920edc79ba79afdf03e39d36d4eeacb206519
Shirley Damiron <shirleydamiron@Administrators-MacBook-Pro.local>
2019-10-10 21:41:27 -0400

    Updates

---
+++ precache-manifest.82cac0f2c0710c1ce26c5461ce771e9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7b61e178bad3b64b5a75748ce20ba6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "948b5685f31474a42819",
+    "url": "/static/css/main.d1a642ee.chunk.css"
+  },
+  {
+    "revision": "4713d9b3a860a65d66d1",
+    "url": "/static/js/2.7d63e76d.chunk.js"
+  },
+  {
+    "revision": "948b5685f31474a42819",
+    "url": "/static/js/main.585ad59f.chunk.js"
+  },
+  {
+    "revision": "f0a2c16a78a76444d565",
+    "url": "/static/js/runtime~main.a290f760.js"
+  }
+]);
